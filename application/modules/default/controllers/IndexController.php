5bc88aa27c0e4e9305e617b3ec28ca93a5f68b2c
John Levermore <john.levermore@zhibek.com>
2015-05-26 14:49:20 +0200

    Implement Doctrine entity/manipulation example.

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -10,7 +10,21 @@ class Default_IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        // Sample code for maniupulating Doctrine below...
+
+        /*
+        $em = $this->getInvokeArg('bootstrap')->getResource('entityManager');
+
+        $entity = new Attendance\Entity\User;
+        $entity->name = 'TEST';
+        $em->persist($entity);
+        $em->flush();
+        var_dump('CREATE', $entity);
+
+        $repository = $em->getRepository('Attendance\Entity\User');
+        $entities = $repository->findAll();
+        var_dump('LIST', $entities);
+        */
     }
 
 
