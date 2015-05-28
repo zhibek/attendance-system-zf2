4f018d41a9abe973ecb58a4b3a455d4f2a28092c
John Levermore <john.levermore@zhibek.com>
2015-05-28 13:49:32 +0200

    Clean up redundant controller code / view templates from default module (active code for this logic is now in the users module).

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,33 +1,12 @@
 <?php
 
-use \Application_Form_Registeration;
-
 class Default_IndexController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
     {
-        // Sample code for maniupulating Doctrine below...
-
-
-//        $em = $this->getInvokeArg('bootstrap')->getResource('entityManager');
-//
-//        $entity = new Attendance\Entity\User;
-//        $entity->name = 'TEST';
-//        $em->persist($entity);
-//        $em->flush();
-//        var_dump('CREATE', $entity);
-//        
-//        $repository = $em->getRepository('Attendance\Entity\User');
-//        $entities = $repository->findAll();
-
-//        $this->view->list = $entities;
-//        var_dump('LIST', $entities);
+        // Default home action
+        // Do nothing
     }
 
 }
