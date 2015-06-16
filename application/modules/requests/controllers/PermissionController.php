fd6449e1398718f70a6c2d8b4f966bf0681fc1ca
moatazelmo <moatazelmohtaseb@gmail.com>
2015-06-16 11:40:18 +0200

    newPermission action + view + model

--- application/modules/requests/controllers/PermissionController.php
+++ application/modules/requests/controllers/PermissionController.php
@@ -9,13 +9,31 @@ class Requests_PermissionController extends Zend_Controller_Action
 {
     public function init() {
         $this->entityManager = $this->getInvokeArg('bootstrap')->getResource('entityManager');
+        $this->permissionModel = new Requests_Model_Permission($this->entityManager);
     }
 
     public function indexAction()
     {
         
+    }
+    
+    public function newAction()
+    {
+        $form = new Requests_Form_PermissionForm(null,$this->entityManager);
         
+        $request = $this->getRequest();
         
+        if ($request->isPost()) 
+        {
+            if ($form->isValid($request->getPost())) 
+            {
+                $permissionInfo = $this->_request->getParams();
+                $this->permissionModel->newPermission($permissionInfo);
+                $this->redirect('/requests/permission/index');
+            }
+        }
+
+        $this->view->form = $form;
     }
 
 }
