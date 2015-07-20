f0dfc16febb9757ae8f63bc52153e31725148ef7
AbdElmoneim Mohamed <abdelmoneim.mohamed@camelcasetech.com>
2015-07-20 15:34:05 +0200

    Branches Module #49

--- application/modules/settings/forms/BranchesForm.php
+++ application/modules/settings/forms/BranchesForm.php
@@ -43,7 +43,7 @@ class Settings_Form_BranchesForm extends Zend_Form
             'required' => true,
             'class' => 'time form-control',
         ));
-        $query = $this->em->createQuery('Select v FROM Attendance\Entity\User  v WHERE v.position = 2');
+        $query = $this->em->createQuery('Select v FROM Attendance\Entity\User  v WHERE v.role = 3');
         $managers = $query->execute();
         foreach ($managers as $p) {
             $this->getElement('manager')->addMultiOption($p->id, $p->name);
