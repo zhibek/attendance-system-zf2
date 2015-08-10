8c6b3dad4d1d157c3d204447eeb260b787eaa0ba
anawany <se_ahmedreda@yahoo.com>
2015-08-10 18:06:08 +0200

    #58 fixing create & edit Holiday And adding notification static module

---
+++ application/modules/notifications/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+class Notifications_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+//        something
+    }
+
+    public function indexAction()
+    {
+        
+    }
+
+}
